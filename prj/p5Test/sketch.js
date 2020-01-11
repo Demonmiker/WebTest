b7cde6561136be31b5510110cb01114873b29b8c
Demonmiker <Demonmiker@ya.ru>
2020-01-11 19:38:59 +0300

    test v2

--- prj/p5Test/sketch.js
+++ prj/p5Test/sketch.js
@@ -2,14 +2,19 @@
 function setup()
 {
     createCanvas(500, 500);
+    background(0, 0, 0);
+    noStroke();
+    
 }
+
+var alpha = 0;
 
 function draw()
 {
-    background(0, 0, 0)
-    stroke(255);
-    if(mouseIsPressed)
-        ellipse(mouseX, mouseY, 20, 20);
+    fill(255, 255, 255, alpha);
+    if (mouseIsPressed) { ellipse(mouseX, mouseY, 20, 20); alpha++ }
+    else { alpha = 0; }
+
 }
    
 
