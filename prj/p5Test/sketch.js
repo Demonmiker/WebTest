1dca5017410ed61729284ba74c6479accc7df053
Demonmiker <Demonmiker@ya.ru>
2020-01-13 13:00:15 +0300

    v0.5

--- prj/p5Test/sketch.js
+++ prj/p5Test/sketch.js
@@ -1,32 +1,49 @@
 // JavaScript source code
 function setup() {
-    var cns = createCanvas(windowWidth - 10, windowHeight - 150);
+    w = windowWidth - 10;
+    h = windowHeight - 150;
+    var cns = createCanvas(w, h);
     cns.parent('canvas_holder')
-
+    
     background(0); //черный фон
-    fill(255, 255, 255);
     noStroke(); // убираю оконтовку у кругов
-    colors = [color('red'), color('magenta'),color('lime'),color('blue'),color('purple')]
+    colors = [color('red'), color('magenta'),color('lime'),color('blue'),color('purple'),color('yellow')]
 }
 
 particles = [];
-
+var w;
+var h;
 var colors = [];
 
 function draw() //всё что дальше происходит каждый кадр
 {
+    background(color(0, 0, 0, 10));
     if (mouseIsPressed) {
         x = mouseX;
         y = mouseY;
-        particles.push(new Particle(mouseX, mouseY));
+        for (let i = 0; i < document.getElementById("tr_par").value;i++)
+            particles.push(new Particle(mouseX, mouseY));
     }
-    for (let p of particles) {
-        p.update();
-        p.show();
+    for (let i = 0; i < particles.length; i++) {
+        if (particles[i].x < 0 || particles[i].x > w || particles[i].y < 0 || particles[i].y > h)
+            particles.splice(i, 1);
+       
     }
+    for (let i = 0; i < particles.length; i++) {
+        particles[i].update();
+        particles[i].show();
+    }
+
+    document.getElementById("out").innerHTML = particles.length
 
 }
+   
 
+var tr_par;
+function tr_var_changed() {
+    tr_par = document.getElementById("tr_par").value;
+    document.getElementById("track").value = tr_par;
+}
 
 
 class Particle {
@@ -36,18 +53,21 @@ class Particle {
         this.x = x;
         this.y = y;
         var v = p5.Vector.random2D();
+        v.setMag(Math.random() * 5);
         this.vx = v.x;
         this.vy = v.y;
         this.ax = 0;
-        this.ay = 0;
+        this.ay = 0.05;
     }
 
     show() {
         fill(this.clr);
-        ellipse(this.x, this.y, 10, 10);
+        ellipse(this.x, this.y, 7, 7);
     }
 
     update() {
+        this.vx += this.ax;
+        this.vy += this.ay;
         this.x += this.vx;
         this.y += this.vy;
     }
